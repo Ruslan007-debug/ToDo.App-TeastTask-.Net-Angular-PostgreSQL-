8c9770f20a20889e10441c825dac873b8298f22c
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-09-11 00:51:26 +0300

    added refresh token to db into User and made DTOs for auth

--- ToDo.Api/ToDo.Api/DataAccess/DTOs/UserDTOs.cs
+++ ToDo.Api/ToDo.Api/DataAccess/DTOs/UserDTOs.cs
@@ -1,4 +1,6 @@
-﻿namespace ToDo.Api.DataAccess.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Api.DataAccess.DTOs
 {
     public class UserDTOs
     {
@@ -6,8 +8,39 @@
         {
             public int Id { get; set; }
             public string Email { get; set; } = string.Empty;
-            public string Password { get; set; } = string.Empty;
 
+        }
+        
+        public class RegisterDTO
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; } = string.Empty;
+            [Required]
+            [MinLength(6)]
+            public string Password { get; set; } = string.Empty;
+        }
+
+        public class LoginDTO
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; } = string.Empty;
+            [Required]
+            public string Password { get; set; } = string.Empty;
+        }
+
+        public class RefreshTokenDTO
+        {
+            [Required]
+            public string RefreshToken { get; set; } = string.Empty;
+        }
+
+        public class TokenResponseDTO
+        {
+            public string AccessToken { get; set; } = string.Empty;
+
+            public string RefreshToken { get; set; } = string.Empty;
         }
     }
 }
--- ToDo.Api/ToDo.Api/DataAccess/Models/User.cs
+++ ToDo.Api/ToDo.Api/DataAccess/Models/User.cs
@@ -5,7 +5,8 @@
         public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
-
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
         public ICollection<Category> Categories { get; set; } = new List<Category>();
         public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
 
