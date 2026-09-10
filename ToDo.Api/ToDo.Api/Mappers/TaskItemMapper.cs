929c8007d5151ed65d33615d7f70220ac4933e4d
Ruslan Terpak <ruxa.terpak@gmail.com>
2026-09-10 15:46:38 +0300

    made TaskItem mapper

---
+++ ToDo.Api/ToDo.Api/Mappers/TaskItemMapper.cs
@@ -0,0 +1,48 @@
+﻿using ToDo.Api.DataAccess.Models;
+using static ToDo.Api.DataAccess.DTOs.TaskItemDTOs;
+
+namespace ToDo.Api.Mappers
+{
+    public static class TaskItemMapper
+    {
+        public static TaskItemDTO ToTaskItemDTO(TaskItem taskItem)
+        {
+            return new TaskItemDTO
+            {
+                Id = taskItem.Id,
+                Title = taskItem.Title,
+                Description = taskItem.Description,
+                IsCompleted = taskItem.IsCompleted,
+                DueDate = taskItem.DueDate,
+                CreatedAt = taskItem.CreatedAt,
+                UserId = taskItem.UserId,
+                CategoryId = taskItem.CategoryId
+            };
+        }
+
+        public static TaskItem ToTaskItemFromCreateDTO(CreateTaskItemDTO dto, int userId)
+        {
+            return new TaskItem
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                IsCompleted = dto.IsCompleted,
+                DueDate = dto.DueDate,
+                UserId = userId,
+                CategoryId = dto.CategoryId
+            };
+        }
+
+        public static TaskItem ToTaskItemFromUpdateDTO(UpdateTaskItemDTO dto)
+        {
+            return new TaskItem
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                IsCompleted = dto.IsCompleted,
+                DueDate = dto.DueDate,
+                CategoryId = dto.CategoryId
+            };
+        }
+    }
+}
